7f8fd7ab1837eda4030c399c6c208092a6bb1487
derespmm <derespmm@miamioh.edu>
2024-11-21 15:43:13 -0500

    flavortexte dits

--- utils.py
+++ utils.py
@@ -23,21 +23,27 @@ def wake_up_flavor_text():
     time.sleep(1)
     cool_print("\nYou feel groggy, your head throbs slightly as you open your eyes.")
     time.sleep(3)
-    cool_print("The faint smell of wood and ash fills the air, and as you sit up, you realize you're in a small, cramped cabin.")
+    cool_print("The faint smell of wood and ash fills the air, and as you sit up, you realize you're in a basement.")
     time.sleep(3)
-    cool_print("The dim light filters through cracks in the floorboard above you, casting shadows across the rough wooden walls.")
+    cool_print("The dim light filters through cracks in the floorboard above you, casting shadows across the rough concrete walls.")
     time.sleep(3)
-    cool_print("You notice a few things around you in this tiny cabin - perhaps you should take a look.\n")
+    cool_print("You notice a few things around you in this tiny room - perhaps you should take a look.\n")
     time.sleep(1)
 
 # Transitional text for trapdoor event
 def cabin_to_forest_transitional_text():
     time.sleep(1)
-    cool_print("\nYou open the trapdoor and a cool breeze greets you from below.")
+    cool_print("\nYou open the trapdoor and a cool breeze greets you from above.")
     time.sleep(2)
-    cool_print("After a moment's hesitation, you descend into the dark passageway.")
+    cool_print("After a moment's hesitation, you ascend the ladder into the cozy, candlelit cabin.")
     time.sleep(3)
-    cool_print("Emerging on the other side, you find yourself in a dense forest with towering trees around.")
+    cool_print("You glance around, and notice a bookshelf against the east wall.")
+    time.sleep(3)
+    cool_print("You look through the books and pick out a heavily annotated notebook.")
+    time.sleep(3)
+    cool_print("The notebook reads about some \"flying ship\" that crashed here a long time ago.")
+    time.sleep(3)
+    cool_print("You put the notebook back in the bookshelf and step outside.\n")
     time.sleep(1)
 
 def ufo_explosion_text():
